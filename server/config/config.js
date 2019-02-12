01b24688ea93e20471aac66172177df9e01f912f
Alberto <rockstar4400@gmail.com>
2019-02-12 14:50:12 -0600

    Google singin produccion

--- server/config/config.js
+++ server/config/config.js
@@ -38,7 +38,7 @@ let urlDB;
 // if (process.env.NODE_ENV === 'dev') {
 //     urlDB = 'mongodb://localhost:27017/cagefe';
 // } else {
-//urlDB = 'mongodb://localhost:monterrey2018@cluster0-shard-00-00-9hiag.gcp.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
-urlDB = 'mongodb://localhost:27017/cafe';
+urlDB = 'mongodb://localhost:monterrey2018@cluster0-shard-00-00-9hiag.gcp.mongodb.net:27017/cafe?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+//urlDB = 'mongodb://localhost:27017/cafe';
 //}
 process.env.URLDB = urlDB;
